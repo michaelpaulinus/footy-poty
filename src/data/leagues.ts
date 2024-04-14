612f1045b12791c7126fc0ea67f0901a7000f546
Michael Paulinus <michael.paulinus@synthesis.co.za>
2024-04-14 13:50:26 +0200

    Updated leagues

--- src/data/leagues.ts
+++ src/data/leagues.ts
@@ -3,24 +3,24 @@ export default [
     name: "England",
     value: 39,
   },
-  {
-    name: "France",
-    value: 61,
-  },
+  // {
+  //   name: "France",
+  //   value: 61,
+  // },
   // {
   //   name: "Germany",
   //   value: 178,
   // },
-  {
-    name: "Italy",
-    value: 135,
-  },
-  {
-    name: "Spain",
-    value: 140,
-  },
-  {
-    name: "UCL",
-    value: 2,
-  },
+  // {
+  //   name: "Italy",
+  //   value: 135,
+  // },
+  // {
+  //   name: "Spain",
+  //   value: 140,
+  // },
+  // {
+  //   name: "UCL",
+  //   value: 2,
+  // },
 ];
