1e76e8c61697e4abdab293f60f9e31f9749fb853
Michael Paulinus <michael.paulinus@synthesis.co.za>
2024-04-14 16:38:40 +0200

    Added more leagues

--- src/data/leagues.ts
+++ src/data/leagues.ts
@@ -1,8 +1,8 @@
 export default [
-  // {
-  //   name: "England",
-  //   value: 39,
-  // },
+  {
+    name: "England",
+    value: 39,
+  },
   {
     name: "France",
     value: 61,
@@ -11,16 +11,16 @@ export default [
   //   name: "Germany",
   //   value: 178,
   // },
-  // {
-  //   name: "Italy",
-  //   value: 135,
-  // },
-  // {
-  //   name: "Spain",
-  //   value: 140,
-  // },
-  // {
-  //   name: "UCL",
-  //   value: 2,
-  // },
+  {
+    name: "Italy",
+    value: 135,
+  },
+  {
+    name: "Spain",
+    value: 140,
+  },
+  {
+    name: "UCL",
+    value: 2,
+  },
 ];
