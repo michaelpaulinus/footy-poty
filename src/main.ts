e87e96de78e80c5f9b5b3f164f26d52d760400da
Michael Paulinus <michael.paulinus@synthesis.co.za>
2024-09-01 22:11:37 +0200

    Removed comments

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
-
-// Vuetify
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+const app = createApp(App);
+
 const vuetify = createVuetify({
-  components,
-  directives,
+	components,
+	directives,
 });
 
-createApp(App).use(vuetify).mount("#app");
+app.use(vuetify).mount("#app");
