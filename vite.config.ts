0f4ca05ac717fb7c4190831974391c093d8d31b5
Michael Paulinus <michael.paulinus@synthesis.co.za>
2024-09-06 10:07:05 +0200

    Added VitePWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,58 @@ import path from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [
+		vue(),
+		vueJsx(),
+		VitePWA({
+			registerType: "autoUpdate",
+			devOptions: {
+				enabled: false,
+			},
+			manifest: {
+				name: "Footy-Poty",
+				short_name: "Footy-Poty",
+				icons: [
+					{
+						src: "/pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "any",
+					},
+					{
+						src: "/pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any",
+					},
+					{
+						src: "/pwa-maskable-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "maskable",
+					},
+					{
+						src: "/pwa-maskable-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "maskable",
+					},
+				],
+				start_url: "/",
+				display: "standalone",
+				background_color: "#FFFFFF",
+				theme_color: "#FFFFFF",
+				description: "Football stats",
+			},
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
